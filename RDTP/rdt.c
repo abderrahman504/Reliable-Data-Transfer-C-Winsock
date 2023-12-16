b60e82893b5d506f49bc8a8303d39ca54c5c5c17
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-16 17:32:06 +0200

    Empty files for RDTP

---
+++ RDTP/rdt.c
@@ -0,0 +1,17 @@
+#include <winsock2.h>
+
+
+
+//Reliable function for sending data through a socket.
+int send(SOCKET socket, char* buffer, int len, int flags)
+{
+
+    return 0;
+}
+
+//Reliable function for receiving data from a socket.
+int rect(SOCKET socket, char* buffer, int len, int flags)
+{
+
+    return 0;
+}
