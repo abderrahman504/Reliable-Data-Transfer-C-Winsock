e31960db6c1bc052a090676774f87c62663f19b5
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-17 18:11:38 +0200

    Rename send and receive methods to avoid naming conflicts

--- RDTP/rdt.c
+++ RDTP/rdt.c
@@ -1,16 +1,32 @@
 #include <winsock2.h>
-
+#include "Segment.c"
+#include <math.h>
 
 
 //Reliable function for sending data through a socket.
-int send(SOCKET socket, char* buffer, int len, int flags)
+int rdt_send(SOCKET socket, char* buffer, int len, int flags)
 {
-
+    //Break up buffer into segments
+    Segment segments[(int)ceil((double)len/MSS)];
+    for(int buf_head=0; buf_head<len; buf_head+=MSS)
+    {
+        Segment segment;
+        segment.type = DATA;
+        segment.len = len-buf_head > MSS ? MSS : len-buf_head;
+        segment.seq = buf_head;
+        for(int i=buf_head; i<buf_head+segment.len; i++)
+        {
+            segment.data[i-buf_head] = buffer[i];
+        }
+        segment.checksum = compute_checksum(&segment);
+        segments[buf_head/MSS] = segment;
+    }
+    
     return 0;
 }
 
 //Reliable function for receiving data from a socket.
-int recv(SOCKET socket, char* buffer, int len, int flags)
+int rdt_recv(SOCKET socket, char* buffer, int len, int flags)
 {
 
     return 0;
