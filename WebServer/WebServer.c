ba4bbe6c627c1225f41fb49620b039348a7a8a42
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-18 16:51:16 +0200

    Remove multiple connection handling code from server

--- WebServer/WebServer.c
+++ WebServer/WebServer.c
@@ -1,108 +1,10 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <process.h>
+// #include <process.h>
 #include "Connection.c"
 #include "../Network.h"
-
-#define MAX_CONNECTIONS 1000
-#define TIMEOUT_1 2000 //Timeout if one connection exists
-//Array of connections
-ConnectionArgs* connections[MAX_CONNECTIONS];
-int cons_head = 0;
-
-long long timeout = TIMEOUT_1;
-int no_of_connections = 0;
-
-
-void listening(SOCKET listen_socket)
-{
-    //Listening on the socket
-    if ( listen( listen_socket, SOMAXCONN ) == SOCKET_ERROR ) {
-        printf( "Listen failed with error: %ld\n", WSAGetLastError() );
-        closesocket(listen_socket);
-        WSACleanup();
-        return;
-    }
-
-    while(1)
-    {
-        //Close any timed out connections and count active connections.
-        no_of_connections = 0;
-        for (int i = 0; i < cons_head; i++)
-        {
-            if (connections[i]->closed) continue; //skip closed connections
-            no_of_connections++;
-            if ((clock() - connections[i]->last_request)/CLOCKS_PER_SEC >= timeout)
-            {
-                printf("Connection timed out\n");
-                shutdown(connections[i]->socket, SD_SEND);
-                closesocket(connections[i]->socket);
-                connections[i]->closed = 1;
-                no_of_connections--;
-            }
-        }
-
-        //update timeout time
-        timeout = no_of_connections == 0 ? TIMEOUT_1 : (TIMEOUT_1 / no_of_connections);
-
-        int pos_for_new_conn;
-        if (cons_head != MAX_CONNECTIONS) pos_for_new_conn = cons_head++;
-        else if (no_of_connections != MAX_CONNECTIONS){
-            //find the first closed connection 
-            for(int i = 0; i < cons_head; i++){
-                if (connections[i]->closed){
-                    pos_for_new_conn = i;
-                    break;
-                }
-            }
-        }
-        
-        if (no_of_connections < MAX_CONNECTIONS) //If not at connection capacity
-        {
-            printf("Listening...\n");
-            no_of_connections++;
-            timeout = TIMEOUT_1 / no_of_connections;
-            accept_connection(listen_socket, pos_for_new_conn, cons_head == MAX_CONNECTIONS);
-        }
-    }
-}
-
-//Accepts a connection and creates a thread to handle it
-void accept_connection(SOCKET listen_socket, int array_pos, int overwrite)
-{
-    struct sockaddr_in clntAddr;
-    int clntAddrLen = sizeof(clntAddr);
-
-    //Accepting a client
-    SOCKET client_socket = INVALID_SOCKET;
-    client_socket = accept(listen_socket, (struct sockaddr*)&clntAddr,  &clntAddrLen);
-    if (client_socket == INVALID_SOCKET) {
-        printf("accept failed: %d\n", WSAGetLastError());
-        return;
-    }
-
-
-    // Get the IP Address of the client
-    char clntName[NI_MAXHOST]; // String to contain client address
-
-    if (getnameinfo((struct sockaddr*)&clntAddr, clntAddrLen, clntName, NI_MAXHOST, NULL, 0, NI_NUMERICHOST) == 0) {
-        printf("Handling client %s\n", clntName);
-    } else {
-        perror("Unable to get client address");
-    }
-
-    
-    ConnectionArgs* args = (ConnectionArgs*) malloc(sizeof(ConnectionArgs));
-    args->socket = client_socket;
-    args->closed = 0;
-    args->last_request = clock();
-    if (overwrite) free(connections[array_pos]);
-    connections[array_pos] = args;
-    printf("Creating thread...\n");
-    _beginthread(connection, 0, (void*)args);
-}
-
+#include "../RDTP/rdt.c"
 
 
 
@@ -133,9 +35,9 @@ int main(char args[])
     }
 
     //Creating a listening socket
-    SOCKET ListenSocket = INVALID_SOCKET;
-    ListenSocket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
-    if (ListenSocket == INVALID_SOCKET) {
+    SOCKET udp_socket = INVALID_SOCKET;
+    udp_socket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
+    if (udp_socket == INVALID_SOCKET) {
         printf("Error at socket(): %ld\n", WSAGetLastError());
         freeaddrinfo(server);
         WSACleanup();
@@ -143,18 +45,29 @@ int main(char args[])
     }
 
     //Binding the socket
-    iResult = bind(ListenSocket, server->ai_addr, (int)server->ai_addrlen);
+    iResult = bind(udp_socket, server->ai_addr, (int)server->ai_addrlen);
     if (iResult == SOCKET_ERROR) {
         printf("bind failed with error: %d\n", WSAGetLastError());
         freeaddrinfo(server);
-        closesocket(ListenSocket);
+        closesocket(udp_socket);
         WSACleanup();
         return 1;
     }
     freeaddrinfo(server);
 
     printf("Server started\n");
-    listening(ListenSocket);
+
+    start(udp_socket);
+
     printf("Server closing\n");
     return 0;
+}
+
+
+void start(SOCKET udp_socket)
+{
+    ConnectionArgs args = {};
+    args.closed = 0;
+    args.socket = udp_socket;
+    connection((void *) &args);
 }
