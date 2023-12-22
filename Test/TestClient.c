5c17f904ccd92d3c31d64fa2dfd7045843992d0a
abderrahman504 <rahmankhsherb@gmail.com>
2023-12-23 01:58:29 +0200

    Test sever and client

---
+++ Test/TestClient.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "../Network.h"
+#include "../RDTP/rdt.c"
+
+
+int main() {
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("Failed to initialize Winsock.\n");
+        return 1;
+    }
+
+    // Create a exchange sockets
+    SOCKET udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (udp_socket == INVALID_SOCKET) {
+        printf("Failed to create send socket.\n");
+        WSACleanup();
+        return 1;
+    }
+
+
+    // Set up server address
+    struct addrinfo *server = NULL, hints;
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family   = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    int iResult = getaddrinfo("localhost", SERVER_PORT, &hints, &server);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
+        return 1;
+    }
+
+
+    // Send data to the server
+    const char* message = "Hello, server!";
+    int messageLength = strlen(message);
+    if (rdt_send(udp_socket, message, messageLength, 0, 0, server->ai_addr, server->ai_addrlen) == SOCKET_ERROR) {
+        printf("Failed to send data to the server.\n");
+        closesocket(udp_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    //Receive data from server
+    char buffer[1024];
+    printf("Receiving data...\n");
+    int bytesRead = rdt_recv(udp_socket, buffer, sizeof(buffer), 0, 0, NULL, NULL);
+    if (bytesRead == SOCKET_ERROR) {
+        printf("Failed to receive data.\n");
+        closesocket(udp_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    printf("Received from server: %s\n", buffer);
+    // Cleanup
+    closesocket(udp_socket);
+    WSACleanup();
+
+    return 0;
+}
---
+++ Test/TestServer.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <process.h>
+#include "../Network.h"
+#include "../RDTP/rdt.c"
+
+
+int main() {
+    // Initialize Winsock
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        printf("Failed to initialize Winsock.\n");
+        return 1;
+    }
+
+    struct addrinfo *server = NULL, hints;
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+    hints.ai_flags = AI_PASSIVE;
+
+    int iResult = getaddrinfo(NULL, SERVER_PORT, &hints, &server);
+    if (iResult != 0) {
+        printf("getaddrinfo failed: %d\n", iResult);
+        WSACleanup();
+        return 1;
+    }
+    // Create a UDP socket
+    SOCKET udp_socket = socket(server->ai_family, SOCK_DGRAM, IPPROTO_UDP);
+    if (udp_socket == INVALID_SOCKET) {
+        printf("Failed to create recv socket.\n");
+        WSACleanup();
+        return 1;
+    }
+
+    //bind UDP socket
+    if (bind(udp_socket, server->ai_addr, server->ai_addrlen) == SOCKET_ERROR) {
+        printf("Failed to bind recv socket.\n");
+        closesocket(udp_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Receive data from the client
+    char buffer[1024];
+    struct sockaddr_in client_addr;
+    int client_addr_len = sizeof(client_addr);
+    printf("Receiving data...\n");
+    int bytesRead = rdt_recv(udp_socket, buffer, sizeof(buffer), 0, 0, (struct sockaddr*)&client_addr, &client_addr_len);
+    if (bytesRead == SOCKET_ERROR) {
+        printf("Failed to receive data.\n");
+        closesocket(udp_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Process the received data
+    printf("Received data from client: %s\n", buffer);
+
+    
+    // Send data to the client
+    const char* message = "Hello, client!";
+    int messageLength = strlen(message);
+    if (rdt_send(udp_socket, message, messageLength, 0, 0, (struct sockaddr*)&client_addr, client_addr_len) == SOCKET_ERROR) {
+        printf("Failed to send data to the server.\n");
+        closesocket(udp_socket);
+        WSACleanup();
+        return 1;
+    }
+
+    // Cleanup
+    closesocket(udp_socket);
+    WSACleanup();
+
+    return 0;
+}
